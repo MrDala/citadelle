1517f7d1642d851e004d8faad60cfe95f6858cf6
Alexis <LEON>
2023-12-09 00:50:52 +0100

    restructuration des tests

---
+++ server/src/modele/joueurs/tests/aJoueur.test.ts
@@ -0,0 +1,5 @@
+// example.test.ts
+test('Always true test', () => {
+  // Le test retourne toujours vrai
+  expect(true).toBe(true);
+});
--- server/tests/modele/regles/ReglesQuatreJoueurs.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@jest/globals';
-
-// example.test.ts
-test('Always true test', () => {
-  // Le test retourne toujours vrai
-  expect(true).toBe(true);
-});
